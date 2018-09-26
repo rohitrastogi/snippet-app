4dc7229cf060a543d61092825d402423233fe7f8
rohitrastogi <rohitrastogi2019@u.northwestern.edu>
2018-09-26 12:39:43 -0500

    setup mongolab

--- api/server.js
+++ api/server.js
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv'
+dotenv.config({ silent: true })
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
@@ -7,6 +9,10 @@ const app = express();
 const router = express.Router();
 
 const API_PORT = process.env.API_PORT || 3001;
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true })
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error!'));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
